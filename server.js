26a880c7b36e6c83b8536fe882a00c4fc36e4a42
AdrianD94 <ady.drozman2007@gmail.com>
2019-02-24 00:35:18 +0200

    adding login,register functionality

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const passport = require('passport');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //Connect to mongo db
-mongoose.connect('mongodb://localhost:27017/SocialMediaApp', {useNewUrlParser: true})
-    .then(()=>console.log('Mongo db connected'))
-    .catch(err=>console.log(err));
+mongoose
+  .connect('mongodb://localhost:27017/SocialMediaApp', {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Mongo db connected'))
+  .catch(err => console.log(err));
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
-app.get('/', (req, res) => res.send('hello world'));
-
-//Use routes 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
-app.use('/api/posts',posts);
+//Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
-
